643019ca235a95b9c150b4757851eb9bb747bb95
RunguoWu <71260597+RunguoWu@users.noreply.github.com>
2025-03-31 17:54:36 +0100

    Add files via upload

---
+++ file_path.R
@@ -0,0 +1,17 @@
+# save shortcut for file paths
+
+# general working directory
+gen_wd <- "D:/OneDrive - Queen Mary, University of London/Work in QMUL/SPCR ovatools"
+
+# R project working directory
+r_wd <- file.path(gen_wd, "github")
+
+# work_data
+work_data <- file.path(r_wd, "working_data")
+
+# output folder
+output <- file.path(r_wd, "output")
+
+# scripts
+scripts <- file.path(r_wd, "scripts")
+
